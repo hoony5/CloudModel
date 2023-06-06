c981cb7cdc2afcb5fdb17441b031a5132e41cb49
hoony5 <changhoony5@gmail.com>
2023-06-06 19:03:28 +0900

    Add

---
+++ DataModel/GameData/Formula/BattleFormulaInfo.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public class BattleFormulaInfo
+{
+    public string Name { get; private set; }
+    public bool UseClampValue { get; private set; }
+    public List<FormulaStat> FormulaStats { get; private set; }
+    public int Min { get; private set; }
+    public int Max { get; private set; }
+    public string Description { get; private set; }
+}
---
+++ DataModel/GameData/Formula/FormulaStat.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+public class FormulaStat
+{
+    public string FormulaName { get; private set; }
+    public string StatusName { get; private set; }
+    public ReflectStatTarget ReflectStatTarget { get; private set; }
+    public float ReflectValue { get; private set; }
+    public DataUnitType DataUnitType { get; private set; }
+    private float[] BaseValues { get; set; }
+    public int Level { get; private set; }
+    public int MaxLevel { get; private set; }
+    public float CalculatedValue { get; private set; }
+}
